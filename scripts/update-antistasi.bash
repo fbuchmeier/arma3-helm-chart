9b7cd584039197440f34eda4deebf8e9df1657d9
Florian Buchmeier <flo_buchmeier@web.de>
2022-10-01 16:55:36 +0200

    feat: run arma3 as non-root user

--- scripts/update-antistasi.bash
+++ scripts/update-antistasi.bash
@@ -10,13 +10,6 @@ cd /arma3/mpmissions
 backup=$(date +%F).mpmissions.tar.gz
 workdir=$(mktemp -d)
 
-for app in curl tar ; do
-  if ! which ${app} ; then
-    apt-get update
-    apt-get install ${app} -y
-  fi
-done
-
 version=$1
 if [ -z $version ] ; then
 	echo "USAGE: $0 antistasi-version
@@ -29,7 +22,7 @@ antistasi=(Antistasi-Altis-VERSION.Altis Antistasi-Anizay-VERSION.tem_anizay Ant
 for i in ${antistasi[@]} ; do
 	dash_version=$(echo $version  | tr '.' '-')
 	name=$(echo $i | sed "s/VERSION/$dash_version/").pbo
-	curl -s --fail -L https://github.com/official-antistasi-community/A3-Antistasi/releases/download/$version/$name --output $workdir/$name
+	wget https://github.com/official-antistasi-community/A3-Antistasi/releases/download/$version/$name -P $workdir/$name
 	[[ $? -ne 0 ]] && echo "Failed downloading $name"
 	find . -name "$(echo $i | sed 's/VERSION.*//')*" -exec rm {} \;
 	mv $workdir/$name $name
